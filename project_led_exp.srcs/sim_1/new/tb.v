ddd0880e0093d0b108ef7e67941682eb81db76d4
spencerwoo <spencerwoo98@foxmail.com>
2019-08-27 19:34:55 +0800

    update: format code

--- project_led_exp.srcs/sim_1/new/tb.v
+++ project_led_exp.srcs/sim_1/new/tb.v
@@ -1,40 +1,39 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 2019/08/27 14:08:26
-// Design Name: 
+// Design Name:
 // Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module tb(
-    );
-	reg clock, rst;
-	initial
-	begin
-		clock = 1'b0;
-        rst = 1'b0;
-	end
-	always #5 clock = ~clock;
-    always #7 rst = 1'b1;
-	wire [7:0] led;
+       );
+reg clock, rst;
+initial begin
+    clock = 1'b0;
+    rst = 1'b0;
+end
+always #5 clock = ~clock;
+always #7 rst = 1'b1;
+wire [7:0] led;
 
-	led_exp  u_led(
-        .clk(clock),
-        .rst(rst),
-        .led(  led   )
-    );
+led_exp  u_led(
+             .clk(clock),
+             .rst(rst),
+             .led(  led   )
+         );
 endmodule
--- project_led_exp.srcs/sources_1/new/led_exp.v
+++ project_led_exp.srcs/sources_1/new/led_exp.v
@@ -1,37 +1,38 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 2019/08/27 11:50:32
-// Design Name: 
+// Design Name:
 // Module Name: led_exp
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 `define RST_ENABLE 0
 module led_exp(
-    input clk,
-    input rst,
-    output reg [7:0]led
-    );
+           input clk,
+           input rst,
+           output reg [7:0]led
+       );
 
 reg[29:0] cnt;
 
 always @ (posedge clk) begin
     if (rst == `RST_ENABLE) begin
         cnt <= 30'b0;
-    end else begin
+    end
+    else begin
         cnt = cnt + 1;
     end
 end
@@ -39,16 +40,25 @@ end
 always @ (posedge clk) begin
     if (rst == `RST_ENABLE) begin
         led <= 8'b0;
-    end else begin
+    end
+    else begin
         case(cnt[28:26])
-        3'b000: led <= 8'b0000_0001;
-        3'b001: led <= 8'b0000_0010;
-        3'b010: led <= 8'b0000_0100;
-        3'b011: led <= 8'b0000_1000;
-        3'b100: led <= 8'b0001_0000;
-        3'b101: led <= 8'b0010_0000;
-        3'b110: led <= 8'b0100_0000;
-        3'b111: led <= 8'b1000_0000;
+            3'b000:
+                led <= 8'b0000_0001;
+            3'b001:
+                led <= 8'b0000_0010;
+            3'b010:
+                led <= 8'b0000_0100;
+            3'b011:
+                led <= 8'b0000_1000;
+            3'b100:
+                led <= 8'b0001_0000;
+            3'b101:
+                led <= 8'b0010_0000;
+            3'b110:
+                led <= 8'b0100_0000;
+            3'b111:
+                led <= 8'b1000_0000;
         endcase
     end
 end
